54d312b2520b30c4a65b659ecef48b7c5218710e
Layssa <layssamoura19@gmail.com>
2023-08-04 20:48:10 -0300

    atualização

--- perfil/views.py
+++ perfil/views.py
@@ -7,3 +7,12 @@ def home(request):
 
 def gerenciar(request):
     return render(request,'gerenciar.html')
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.POST.get('icone')
+    
+    

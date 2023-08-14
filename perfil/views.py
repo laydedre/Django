8a60f21a3e92a18dd8af56a8cdc3bcffb133b3a5
Layssa <layssamoura19@gmail.com>
2023-08-14 20:34:35 -0300

    atualização

--- perfil/views.py
+++ perfil/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from perfil.models import Conta
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 # Create your views here.

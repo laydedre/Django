8a686e0e78e14a3af238f1e71b35345349ca7854
Layssa <layssamoura19@gmail.com>
2023-08-15 20:21:42 -0300

    atualização

--- perfil/views.py
+++ perfil/views.py
@@ -10,10 +10,11 @@ def home(request):
 
 def gerenciar(request):
     contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
     total_contas = 0
     for conta in contas:
         total_contas += conta.valor
-    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas,})
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': categorias,})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')

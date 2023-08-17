933aada04ea59495b2bd4315c44280b9fad37fdf
Layssa <layssamoura19@gmail.com>
2023-08-17 19:49:55 -0300

    atualização

--- extrato/views.py
+++ extrato/views.py
@@ -1,9 +1,15 @@
+from io import BytesIO
+import os
+from weasyprint import HTML
+from django.http import FileResponse
 from django.contrib.messages import constants
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.conf import settings
 from extrato.models import Valores
 from perfil.models import Categoria, Conta
 from datetime import datetime
+from django.template.loader import render_to_string
 
 def novo_valor(request):
     if request.method == "GET":
@@ -62,3 +68,19 @@ def view_extrato(request):
  
 
     return render(request, 'view_extrato.html', {'valores': valores, 'contas': contas, 'categorias': categorias})
+
+def exportar_pdf(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+   
+    path_template = os.path.join(settings.BASE_DIR, 'templates/partials/extrato.html')
+    path_output = BytesIO()
+
+    template_render = render_to_string(path_template, {'valores': valores, 'contas': contas, 'categorias': categorias})
+    HTML(string=template_render).write_pdf(path_output)
+
+    path_output.seek(0)
+   
+
+    return FileResponse(path_output, filename="extrato.pdf")

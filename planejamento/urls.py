db36a49076304dcb6b137f2da45cd2f0b48723dd
Layssa <layssamoura19@gmail.com>
2023-08-18 20:08:25 -0300

    atualização

--- planejamento/urls.py
+++ planejamento/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('definir_planejamento', views.definir_planejamento, name="definir_planejamento"),
+        path('definir_planejamento/', views.definir_planejamento, name="definir_planejamento"),
         
 ]

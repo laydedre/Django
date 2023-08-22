5cf4b24e18e2488bef0d73c423d59fd4289868a1
Layssa <layssamoura19@gmail.com>
2023-08-22 20:19:48 -0300

    atualização

--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render,redirect
 from contas.models import ContaPagar,ContaPaga
 from django.contrib import messages
@@ -27,3 +28,19 @@ def definir_contas(request):
 
         messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
         return redirect('/contas/definir_contas')
+    
+def ver_contas(request):
+    MES_ATUAL = datetime.now().month
+    DIA_ATUAL = datetime.now().day
+   
+    contas = ContaPagar.objects.all()
+
+    contas_pagas = ContaPaga.objects.filter(data_pagamento__month=MES_ATUAL).values('conta')
+
+    contas_vencidas = contas.filter(dia_pagamento__lt=DIA_ATUAL).exclude(id__in=contas_pagas)
+   
+    contas_proximas_vencimento = contas.filter(dia_pagamento__lte = DIA_ATUAL + 5).filter(dia_pagamento__gte=DIA_ATUAL).exclude(id__in=contas_pagas)
+   
+    restantes = contas.exclude(id__in=contas_vencidas).exclude(id__in=contas_pagas).exclude(id__in=contas_proximas_vencimento)
+
+    return render(request, 'ver_contas.html', {'contas_vencidas': contas_vencidas, 'contas_proximas_vencimento': contas_proximas_vencimento, 'restantes': restantes})

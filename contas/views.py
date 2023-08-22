afa373191fc475ace64dd1db690f071c2e8a126e
Layssa <layssamoura19@gmail.com>
2023-08-22 20:14:21 -0300

    atualização

--- contas/views.py
+++ contas/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from contas.models import ContaPagar,ContaPaga
+from django.contrib import messages
+from django.contrib.messages import constants
 from perfil.models import Categoria
 
 def definir_contas(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         categorias = Categoria.objects.all()
-        return render(request, 'definir_contas.html', {'categorias':categorias})
+        return render(request, 'definir_contas.html', {'categorias': categorias})
+    else:
+        titulo = request.POST.get('titulo')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        valor = request.POST.get('valor')
+        dia_pagamento = request.POST.get('dia_pagamento')
+
+        conta = ContaPagar(
+            titulo=titulo,
+            categoria_id=categoria,
+            descricao=descricao,
+            valor=valor,
+            dia_pagamento=dia_pagamento
+        )
+
+        conta.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
+        return redirect('/contas/definir_contas')
